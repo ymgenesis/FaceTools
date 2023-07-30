3173df7869b91f97b115cb7ee5ca189ae8d09aac
ymgenesis <janik.matthew@gmail.com>
2023-07-30 18:38:01 +0200

    add FaceID to FaceOff
    - Add FaceID feature to FaceOff
    - One FaceID can be given at a time to select a face. Use FaceIdentifier to find a face's ID in an image.
    - Added more checks and console outputs

--- faceoff.py
+++ faceoff.py
@@ -1,4 +1,4 @@
-## FaceOff 3.0
+## FaceOff 3.1
 ## A node for InvokeAI, written by YMGenesis/Matthew Janik
 
 from typing import Literal, Optional
@@ -51,11 +51,14 @@ class FaceOffInvocation(BaseInvocation, PILInvocationConfig):
     type: Literal["face_off"] = "face_off"
 
     # Inputs
-    image:           Optional[ImageField]  = Field(default=None, description="Image for face detection")
-    x_offset:        float = Field(default=0.0, description="X-axis offset of the mask")
-    y_offset:        float = Field(default=0.0, description="Y-axis offset of the mask")
-    padding:         int = Field(default=0, description="All-axis padding around the mask in pixels")
-    scale_factor:    int = Field(default=2, description="Factor to scale the bounding box by before outputting")
+    image:               Optional[ImageField]  = Field(default=None, description="Image for face detection")
+    face_id:             int = Field(default=0, description="0 for first detected face, single digit for one specific. Multiple faces not supported. Find a face's ID with FaceIdentifier node.")
+    faces:               int = Field(default=4, description="Maximum number of faces to detect")
+    minimum_confidence:  float = Field(default=0.5, description="Minimum confidence for face detection (lower if detection is failing)")
+    x_offset:            float = Field(default=0.0, description="X-axis offset of the mask")
+    y_offset:            float = Field(default=0.0, description="Y-axis offset of the mask")
+    padding:             int = Field(default=0, description="All-axis padding around the mask in pixels")
+    scale_factor:        int = Field(default=2, description="Factor to scale the bounding box by before outputting")
     # fmt: on
 
     class Config(InvocationConfig):
@@ -76,14 +79,37 @@ class FaceOffInvocation(BaseInvocation, PILInvocationConfig):
             np_image = np_image[:, :, :3]
 
         # Create a FaceMesh object for face landmark detection and mesh generation.
-        face_mesh = mp.solutions.face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+        face_mesh = mp.solutions.face_mesh.FaceMesh(
+            max_num_faces=self.faces,
+            min_detection_confidence=self.minimum_confidence,
+            min_tracking_confidence=self.minimum_confidence
+        )
 
         # Detect the face landmarks and mesh in the input image.
         results = face_mesh.process(np_image)
 
         # Check if any face is detected.
         if results.multi_face_landmarks:
-            face_landmarks = results.multi_face_landmarks[0]  # Assuming only one face is detected.
+            face_id_counter = 1  # Start face ID counter from 1
+            # If face_id is higher than faces to detect
+            if self.face_id > self.faces:
+                raise ValueError("Requested Face ID is higher than maximum faces to detect. Increase the Faces input value.")
+            # If face_id index # is out of results.multi_face_landmarks scope
+            elif not (0 <= self.face_id < (len(results.multi_face_landmarks) + 1)):
+                raise ValueError("Requested Face ID is not in the list of detected faces.")
+            # If face_id is set to 0, use the first detected face.
+            elif self.face_id == 0:
+                face_landmarks = results.multi_face_landmarks[0]  # Use the first detected face.
+            else:
+                # Search for the face_id in the detected faces.
+                for face_landmarks in results.multi_face_landmarks:
+                    if face_id_counter == self.face_id:
+                        break
+                    face_id_counter += 1
+
+                # Check if the requested face_id is found.
+                if face_id_counter != self.face_id:
+                    raise ValueError("Requested face ID not found in the detected faces.")
 
             # Get the bounding box of the face mesh.
             x_coordinates = [landmark.x for landmark in face_landmarks.landmark]
@@ -101,20 +127,19 @@ class FaceOffInvocation(BaseInvocation, PILInvocationConfig):
 
             # Generate a binary face mask using the face mesh.
             mask_image = np.ones(np_image.shape[:2], dtype=np.uint8) * 255
-            for face_landmarks in results.multi_face_landmarks:
-                face_landmark_points = np.array(
-                    [[landmark.x * np_image.shape[1], landmark.y * np_image.shape[0]] for landmark in face_landmarks.landmark]
-                )
+            face_landmark_points = np.array(
+                [[landmark.x * np_image.shape[1], landmark.y * np_image.shape[0]] for landmark in face_landmarks.landmark]
+            )
 
-                # Apply the scaling offsets to the face landmark points with a multiplier.
-                scale_multiplier = 0.2
-                x_center = np.mean(face_landmark_points[:, 0])
-                y_center = np.mean(face_landmark_points[:, 1])
-                x_scaled = face_landmark_points[:, 0] + scale_multiplier * self.x_offset * (face_landmark_points[:, 0] - x_center)
-                y_scaled = face_landmark_points[:, 1] + scale_multiplier * self.y_offset * (face_landmark_points[:, 1] - y_center)
+            # Apply the scaling offsets to the face landmark points with a multiplier.
+            scale_multiplier = 0.2
+            x_center = np.mean(face_landmark_points[:, 0])
+            y_center = np.mean(face_landmark_points[:, 1])
+            x_scaled = face_landmark_points[:, 0] + scale_multiplier * self.x_offset * (face_landmark_points[:, 0] - x_center)
+            y_scaled = face_landmark_points[:, 1] + scale_multiplier * self.y_offset * (face_landmark_points[:, 1] - y_center)
 
-                convex_hull = cv2.convexHull(np.column_stack((x_scaled, y_scaled)).astype(np.int32))
-                cv2.fillConvexPoly(mask_image, convex_hull, 0)
+            convex_hull = cv2.convexHull(np.column_stack((x_scaled, y_scaled)).astype(np.int32))
+            cv2.fillConvexPoly(mask_image, convex_hull, 0)
 
             # Convert the binary mask image to a PIL Image.
             mask_pil = Image.fromarray(mask_image, mode='L')
@@ -123,7 +148,6 @@ class FaceOffInvocation(BaseInvocation, PILInvocationConfig):
 
         else:
             raise ValueError("No face detected in the input image.")
-            context.services.logger.warning('No face detected in the input image.')
 
     def invoke(self, context: InvocationContext) -> FaceOffOutput:
         image = context.services.images.get_pil_image(self.image.image_name)
@@ -149,42 +173,42 @@ class FaceOffInvocation(BaseInvocation, PILInvocationConfig):
 
         # Adjust the crop boundaries to stay within the original image's dimensions
         if x_min < 0:
-            context.services.logger.warning(f'Padding reached image edge.')
+            context.services.logger.warning(f'FaceOff --> -X-axis padding reached image edge.')
             x_max -= x_min
             x_min = 0
         elif x_max > mask_pil.width:
-            context.services.logger.warning(f'Padding reached image edge.')
+            context.services.logger.warning(f'FaceOff --> +X-axis padding reached image edge.')
             x_min -= (x_max - mask_pil.width)
             x_max = mask_pil.width
 
         if y_min < 0:
-            context.services.logger.warning(f'Padding reached image edge.')
+            context.services.logger.warning(f'FaceOff --> +Y-axis padding reached image edge.')
             y_max -= y_min
             y_min = 0
         elif y_max > mask_pil.height:
-            context.services.logger.warning(f'Padding reached image edge.')
+            context.services.logger.warning(f'FaceOff --> -Y-axis padding reached image edge.')
             y_min -= (y_max - mask_pil.height)
             y_max = mask_pil.height
 
         # Ensure the crop is square and adjust the boundaries if needed
         if x_max - x_min != crop_size:
-            context.services.logger.warning(f'Limiting padding to constrain bounding box to a square.')
+            context.services.logger.warning(f'FaceOff --> Limiting x-axis padding to constrain bounding box to a square.')
             diff = crop_size - (x_max - x_min)
             x_min -= diff // 2
             x_max += diff - diff // 2
 
         if y_max - y_min != crop_size:
-            context.services.logger.warning(f'Limiting padding to constrain bounding box to a square.')
+            context.services.logger.warning(f'FaceOff --> Limiting y-axis padding to constrain bounding box to a square.')
             diff = crop_size - (y_max - y_min)
             y_min -= diff // 2
             y_max += diff - diff // 2
 
-        context.services.logger.info(f'Calculated bounding box (8 multiple): {crop_size}')
-        context.services.logger.info(f'Scale factor: {self.scale_factor}')
+        context.services.logger.info(f'FaceOff --> Calculated bounding box (8 multiple): {crop_size}')
+        context.services.logger.info(f'FaceOff --> Scale factor: {self.scale_factor}')
         if self.scale_factor == 0:
-            context.services.logger.info(f'Scaled bounding box: {crop_size}')
+            context.services.logger.info(f'FaceOff --> Scaled bounding box: {crop_size}')
         else:
-            context.services.logger.info(f'Scaled bounding box: {crop_size * self.scale_factor}')
+            context.services.logger.info(f'FaceOff --> Scaled bounding box: {crop_size * self.scale_factor}')
 
         # Crop the output image to the specified size with the center of the face mesh as the center.
         mask_pil = mask_pil.crop((x_min, y_min, x_max, y_max))
