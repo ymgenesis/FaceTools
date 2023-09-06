22fc90a33022923aa1595920e84eeefac5589f9a
Jonathan <34005131+JPPhoto@users.noreply.github.com>
2023-09-06 10:22:45 -0500

    Update faceoff.py
    Use a new strategy for finding and picking faces, sorting primarily by x and then y.

--- faceoff.py
+++ faceoff.py
@@ -5,6 +5,7 @@ import numpy as np
 import mediapipe as mp
 from PIL import Image, ImageFilter
 import cv2
+import math
 from invokeai.app.models.image import (ImageCategory, ResourceOrigin)
 from invokeai.app.invocations.primitives import ImageField
 from invokeai.app.invocations.baseinvocation import (
@@ -16,6 +17,35 @@ from invokeai.app.invocations.baseinvocation import (
     invocation,
     invocation_output)
 
+
+def cleanup_faces_list(orig):
+    newlist = []
+    for i in orig:
+        should_add = True
+        i_x_center = i["x_center"]
+        i_y_center = i["y_center"]
+        for j in newlist:
+            face_center_x = j["x_center"]
+            face_center_y = j["y_center"]
+            face_radius_w = j["mesh_width"] / 2
+            face_radius_h = j["mesh_height"] / 2
+            # Determine if the center of the candidate i is inside the ellipse of the added face
+            # p < 1 -> Inside
+            # p = 1 -> Exactly on the ellipse
+            # p > 1 -> Outside
+            p = ((math.pow((i_x_center - face_center_x), 2) / math.pow(face_radius_w, 2)) +
+                 (math.pow((i_y_center - face_center_y), 2) / math.pow(face_radius_h, 2)))
+
+            if p < 1: # Inside of the already-added face's radius
+                should_add = False
+                break
+
+        if should_add is True:
+            newlist.append(i)
+
+    newlist = sorted(newlist, key=lambda x: x['y_center'])
+    newlist = sorted(newlist, key=lambda x: x['x_center'])
+    return newlist
 
 @invocation_output("face_off_output")
 class FaceOffOutput(BaseInvocationOutput):
@@ -35,14 +65,16 @@ class FaceOffInvocation(BaseInvocation):
 
     image:               ImageField  = InputField(description="Image for face detection")
     face_id:             int = InputField(default=0, description="0 for first detected face, single digit for one specific. Multiple faces not supported. Find a face's ID with FaceIdentifier node.")
-    faces:               int = InputField(default=4, description="Maximum number of faces to detect")
+    faces:               int = InputField(default=99, description="Maximum number of faces to detect")
     minimum_confidence:  float = InputField(default=0.5, description="Minimum confidence for face detection (lower if detection is failing)")
     x_offset:            float = InputField(default=0.0, description="X-axis offset of the mask")
     y_offset:            float = InputField(default=0.0, description="Y-axis offset of the mask")
     padding:             int = InputField(default=0, description="All-axis padding around the mask in pixels")
     scale_factor:        int = InputField(default=2, description="Factor to scale the bounding box by before outputting")
 
-    def generate_face_box_mask(self, pil_image):
+    def generate_face_box_mask(self, pil_image, x_offset=0, y_offset=0):
+        result = []
+
         # Convert the PIL image to a NumPy array.
         np_image = np.array(pil_image, dtype=np.uint8)
 
@@ -63,65 +95,52 @@ class FaceOffInvocation(BaseInvocation):
 
         # Check if any face is detected.
         if results.multi_face_landmarks:
-            face_id_counter = 1  # Start face ID counter from 1 (TBD: See how this works for split-up images)
-            # If face_id is higher than faces to detect
-            if self.face_id > self.faces:
-                raise ValueError("Requested Face ID is higher than maximum faces to detect. Increase the Faces input value.")
-            # If face_id index # is out of results.multi_face_landmarks scope
-            elif not (0 <= self.face_id < (len(results.multi_face_landmarks) + 1)):
-                raise ValueError("Requested Face ID is not in the list of detected faces.")
-            # If face_id is set to 0, use the first detected face.
-            elif self.face_id == 0:
-                face_landmarks = results.multi_face_landmarks[0]  # Use the first detected face.
-            else:
-                # Search for the face_id in the detected faces.
-                for face_landmarks in results.multi_face_landmarks:
-                    if face_id_counter == self.face_id:
-                        break
-                    face_id_counter += 1
+            # Search for the face_id in the detected faces.
+            for face_landmarks in results.multi_face_landmarks:
 
-                # Check if the requested face_id is found.
-                if face_id_counter != self.face_id:
-                    raise ValueError("Requested face ID not found in the detected faces.")
+                # Get the bounding box of the face mesh.
+                x_coordinates = [landmark.x for landmark in face_landmarks.landmark]
+                y_coordinates = [landmark.y for landmark in face_landmarks.landmark]
+                x_min, x_max = min(x_coordinates), max(x_coordinates)
+                y_min, y_max = min(y_coordinates), max(y_coordinates)
 
-            # Get the bounding box of the face mesh.
-            x_coordinates = [landmark.x for landmark in face_landmarks.landmark]
-            y_coordinates = [landmark.y for landmark in face_landmarks.landmark]
-            x_min, x_max = min(x_coordinates), max(x_coordinates)
-            y_min, y_max = min(y_coordinates), max(y_coordinates)
+                # Calculate the width and height of the face mesh.
+                mesh_width = int((x_max - x_min) * np_image.shape[1])
+                mesh_height = int((y_max - y_min) * np_image.shape[0])
 
-            # Calculate the width and height of the face mesh.
-            mesh_width = int((x_max - x_min) * np_image.shape[1])
-            mesh_height = int((y_max - y_min) * np_image.shape[0])
+                # Get the center of the face.
+                x_center = np.mean([landmark.x * np_image.shape[1] for landmark in face_landmarks.landmark])
+                y_center = np.mean([landmark.y * np_image.shape[0] for landmark in face_landmarks.landmark])
 
-            # Get the center of the face.
-            x_center = np.mean([landmark.x * np_image.shape[1] for landmark in face_landmarks.landmark])
-            y_center = np.mean([landmark.y * np_image.shape[0] for landmark in face_landmarks.landmark])
+                # Generate a binary face mask using the face mesh.
+                mask_image = np.ones(np_image.shape[:2], dtype=np.uint8) * 255
+                face_landmark_points = np.array(
+                    [[landmark.x * np_image.shape[1], landmark.y * np_image.shape[0]] for landmark in face_landmarks.landmark]
+                )
 
-            # Generate a binary face mask using the face mesh.
-            mask_image = np.ones(np_image.shape[:2], dtype=np.uint8) * 255
-            face_landmark_points = np.array(
-                [[landmark.x * np_image.shape[1], landmark.y * np_image.shape[0]] for landmark in face_landmarks.landmark]
-            )
+                # Apply the scaling offsets to the face landmark points with a multiplier.
+                scale_multiplier = 0.2
+                x_center = np.mean(face_landmark_points[:, 0])
+                y_center = np.mean(face_landmark_points[:, 1])
+                x_scaled = face_landmark_points[:, 0] + scale_multiplier * self.x_offset * (face_landmark_points[:, 0] - x_center)
+                y_scaled = face_landmark_points[:, 1] + scale_multiplier * self.y_offset * (face_landmark_points[:, 1] - y_center)
 
-            # Apply the scaling offsets to the face landmark points with a multiplier.
-            scale_multiplier = 0.2
-            x_center = np.mean(face_landmark_points[:, 0])
-            y_center = np.mean(face_landmark_points[:, 1])
-            x_scaled = face_landmark_points[:, 0] + scale_multiplier * self.x_offset * (face_landmark_points[:, 0] - x_center)
-            y_scaled = face_landmark_points[:, 1] + scale_multiplier * self.y_offset * (face_landmark_points[:, 1] - y_center)
+                convex_hull = cv2.convexHull(np.column_stack((x_scaled, y_scaled)).astype(np.int32))
+                cv2.fillConvexPoly(mask_image, convex_hull, 0)
 
-            convex_hull = cv2.convexHull(np.column_stack((x_scaled, y_scaled)).astype(np.int32))
-            cv2.fillConvexPoly(mask_image, convex_hull, 0)
+                # Convert the binary mask image to a PIL Image.
+                mask_pil = Image.fromarray(mask_image, mode='L')
 
-            # Convert the binary mask image to a PIL Image.
-            mask_pil = Image.fromarray(mask_image, mode='L')
+                this_face = dict()
+                this_face["pil_image"] = pil_image
+                this_face["mask_pil"] = mask_pil
+                this_face["x_center"] = x_center + x_offset
+                this_face["y_center"] = y_center + y_offset
+                this_face["mesh_width"] = mesh_width
+                this_face["mesh_height"] = mesh_height
+                result.append(this_face)
 
-            return mask_pil, x_center, y_center, mesh_width, mesh_height
-
-        else:
-            print("No face detected!")
-            raise ValueError("No face detected in the input image.")
+        return result
 
     def faceoff(self, context: InvocationContext) -> FaceOffOutput:
         image = context.services.images.get_pil_image(self.image.image_name)
@@ -129,9 +148,8 @@ class FaceOffInvocation(BaseInvocation):
         y_chunk_off = 0
 
         # Generate the face box mask and get the center of the face.
-        try:
-            mask_pil, center_x, center_y, mesh_width, mesh_height = self.generate_face_box_mask(image)
-        except ValueError:
+        result = self.generate_face_box_mask(image)
+        if len(result) == 0:
             print("Chunking image")
             width, height = image.size
             image_chunks = []
@@ -167,23 +185,27 @@ class FaceOffInvocation(BaseInvocation):
             found = False
 
             for idx in range(len(image_chunks)):
-                try:
-                    print(f"Trying chunk {idx}")
-                    chunk = image_chunks[idx]
-                    mask_pil, center_x, center_y, mesh_width, mesh_height = self.generate_face_box_mask(chunk)
-                    image = chunk
-                    x_chunk_off = x_offsets[idx]
-                    y_chunk_off = y_offsets[idx]
-                    found = True
-                    break
-                except ValueError:
-                    # Nothing found, so keep going.
-                    pass
+                print(f"Trying chunk {idx}")
+                result = result + self.generate_face_box_mask(image_chunks[idx], x_offsets[idx], y_offsets[idx])
 
-            if found is False:
+            if len(result) == 0:
                 # Give up
                 print(f"No face detected in chunked input image. Passing through original image. Resizing by a factor of {self.scale_factor}...")
-                raise
+                raise ValueError("No face detected in input image.")
+
+        result = cleanup_faces_list(result)
+
+        face_id = self.face_id - 1 if self.face_id > 0 else self.face_id
+
+        if face_id > len(result) or face_id < 0:
+            raise ValueError("Selected face ID is outside of the number of faces detected.")
+
+        image = result[face_id]["pil_image"]
+        mask_pil = result[face_id]["mask_pil"]
+        center_x = result[face_id]["x_center"]
+        center_y = result[face_id]["y_center"]
+        mesh_width = result[face_id]["mesh_width"]
+        mesh_height = result[face_id]["mesh_height"]
 
         # Determine the minimum size of the square crop
         min_size = min(mask_pil.width, mask_pil.height)
@@ -294,7 +316,7 @@ class FaceOffInvocation(BaseInvocation):
                 new_size = (image.width * self.scale_factor, image.height * self.scale_factor)
                 image = image.resize(new_size, resample=Image.LANCZOS)
                 whitemask = whitemask.resize(new_size, resample=Image.LANCZOS)
-
+                                          
             image_dto = context.services.images.create(
                 image=image,
                 image_origin=ResourceOrigin.INTERNAL,
@@ -321,3 +343,4 @@ class FaceOffInvocation(BaseInvocation):
                 x=0,
                 y=0,
             )
+            
